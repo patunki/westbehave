36fa053c5be32f69b86f9331233afb7f942f9f78
patunki <paulus.haapala@proton.me>
2024-10-22 22:30:25 +0300

    Godot update

--- Items/Classes/Food.cs
+++ Items/Classes/Food.cs
@@ -4,21 +4,21 @@ using System.Collections;
 
 public partial class Food : Item
 {
-    [Export]
-    public int FoodValue;
-    [Export]
-    public int WaterValue;
-    [Export]
-    public bool CanCook;
-    [Export]
-    public Item CookedItem;
+	[Export]
+	public int FoodValue;
+	[Export]
+	public int WaterValue;
+	[Export]
+	public bool CanCook;
+	[Export]
+	public Item CookedItem;
 
-    public int Eat(){
-        DecQuant();
-        return FoodValue;
-    }
-    public int Drink(){
-        return WaterValue;
-    }
-    
+	public int Eat(){
+		DecQuant();
+		return FoodValue;
+	}
+	public int Drink(){
+		return WaterValue;
+	}
+	
 }
--- Player/EquippedItem.cs
+++ Player/EquippedItem.cs
@@ -3,28 +3,28 @@ using System;
 
 public partial class EquippedItem : Node2D
 {
-    [Export]
-    Entity entity;
-    Node2D Game;
+	[Export]
+	Entity entity;
+	Node2D Game;
 	PackedScene packedScene;
 	ItemScene itemInstance;
-    HungerComponent hungerComponent;
-    Inventory inventory;
-    public Item equipItem;
-    TextureRect equipTexture;
+	HungerComponent hungerComponent;
+	Inventory inventory;
+	public Item equipItem;
+	TextureRect equipTexture;
 	[Export]
 	Hotbar hotbar;
 	bool sceneAlive = false;
 
-    public override void _Ready()
-    {
-        Game = GetNode<Node2D>("/root/Game");
-        hungerComponent = entity.GetNode<HungerComponent>("HungerComponent");
-        inventory = entity.inventory;
-        equipTexture = GetNode<TextureRect>("EquipTexture");
-        inventory.InventoryChanged += UpdateItem;
-    }
-    public void UpdateItem(){
+	public override void _Ready()
+	{
+		Game = GetNode<Node2D>("/root/Game");
+		hungerComponent = entity.GetNode<HungerComponent>("HungerComponent");
+		inventory = entity.inventory;
+		equipTexture = GetNode<TextureRect>("EquipTexture");
+		inventory.InventoryChanged += UpdateItem;
+	}
+	public void UpdateItem(){
 		int selected = hotbar.selected;
 		if (inventory.InventoryItems[selected] == equipItem){
 			return;
@@ -55,31 +55,31 @@ public partial class EquippedItem : Node2D
 		
 	}
 
-    public override void _Input(InputEvent @event)
-    {
+	public override void _Input(InputEvent @event)
+	{
 		if (equipItem != null && !equipItem.HAS_SCENE && Input.IsActionJustPressed("interact")){
 			equipItem.Call("OnInteract",GlobalPosition);
 		}
 
 		if (Input.IsActionJustPressed("drop") && equipItem != null){
-                
-                PackedScene dropItem = GD.Load("res://Scenes/DroppedItem.tscn") as PackedScene;
-                DroppedItem instance = (DroppedItem)dropItem.Instantiate();
+				
+				PackedScene dropItem = GD.Load("res://Scenes/DroppedItem.tscn") as PackedScene;
+				DroppedItem instance = (DroppedItem)dropItem.Instantiate();
 
-                instance.item = equipItem.Duplicate() as Item;
-                instance.Position = GlobalPosition;
-                Game.AddChild(instance);
+				instance.item = equipItem.Duplicate() as Item;
+				instance.Position = GlobalPosition;
+				Game.AddChild(instance);
 
-                equipItem.DecQuant();
+				equipItem.DecQuant();
 				
 		}
 
-    }
+	}
 
-    public override void _Process(double delta)
-    {
-        inventory.NullItemCheck();
-    }
+	public override void _Process(double delta)
+	{
+		inventory.NullItemCheck();
+	}
 
 
 }
